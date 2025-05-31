7e523f7708557b80554703834d490d01ea88fb69
namitaML <namita.saket@gmail.com>
2025-05-31 15:03:27 +0530

    test:wq

--- prime.py
+++ prime.py
@@ -11,7 +11,7 @@ def generate_primes(n):
     is_prime = [True] * (n + 1)
     is_prime[0] = is_prime[1] = False
     
-    for i in range(2, int(n ** 0.4) + 1):
+    for i in range(2, int(n ** 0.5) + 1):
         if is_prime[i]:
             # Update all multiples of i
             for j in range(i * i, n + 1, i):
